c4d9259687bdaef44ed52d9c08892f6b78b842f7
mishkago <mishkagoroshko@gmail.com>
2025-01-17 18:43:43 +0300

    Update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 import re
 import os
@@ -50,5 +51,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
